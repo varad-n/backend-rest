58f91c30b7958e9127ed66b461d21c334483dec5
varad-n <hvi924926@gmail.com>
2025-08-07 17:18:16 +0530

    in progress

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/subscribers')
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to database'))
+
+app.listen(3000, () => 
+    console.log("Server started"))
