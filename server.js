5c08a8e6bc9fd1761e338693a2035c269064493f
varad-n <hvi924926@gmail.com>
2025-08-08 23:59:31 +0530

    in progress

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect('mongodb://localhost/subscribers')
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to database'))
+
+app.use(express.json())
 
 app.listen(3000, () => 
     console.log("Server started"))
