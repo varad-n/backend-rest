27e7f20431c191915799fc197c62cd03ac85d176
varad-n <hvi924926@gmail.com>
2025-08-11 18:12:05 +0530

    REST API done!!!

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use(express.json())
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
-app.listen(3000, () => 
+app.listen(3000 , () => 
     console.log("Server started"))
